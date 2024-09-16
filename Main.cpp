12054c7dff7476cfcf3e1baad4d35cea8e834321
user <user@mail.ru>
2024-09-16 21:33:21 +0300

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <memory>
+#include "shared_ptr.hpp"
+
+
+
+int main() {
+	setlocale(LC_ALL, "russian");
+	int n;
+
+	//Умный указатель на уникальную память
+	/*
+
+	n = 15;
+	int* ptmp = new int{ 7 };
+	//std::unique_ptr<int> uniq1(&n);
+	std::unique_ptr<int> uniq2(ptmp);
+	//std::cout << "*uniq1 = " << *uniq1 << std::endl;
+	std::cout << "*uniq2 = " << *uniq2 << std::endl;
+	uniq2.reset(new int[3]);
+	uniq2.get();
+
+	auto uniq5(std::make_unique<int>(10));
+
+*/
+
+	//Общий указатель
+	/*
+	std::shared_ptr<int> shared1(new int);
+	std::shared_ptr<int> shared2 = shared1;
+	*shared1 = 50;
+	std::cout << *shared2 << std::endl;
+*/
+
+	//Слабый указатель
+	std::weak_ptr<int> weak(std::make_shared<int>(50));
+	//std::weak_ptr<int> weak(std::make_weak<int>(50))  //Ошибка
+
+
+	//проверка собственного общего указателя
+	my::shared_ptr_test(new int{ 10 });
+
+	return 0;
+}
---
+++ shared_ptr.hpp
@@ -0,0 +1,114 @@
+﻿#pragma once
+#include<map>
+#include <iostream>
+
+namespace my{
+	template <typename ANY>
+class Shared_ptr {
+private:
+	ANY* data;
+	std::map<ANY*, unsigned> all_ptrs;
+	void clear();
+public:
+	Shared_ptr();
+	Shared_ptr(ANY* address);
+	~Shared_ptr();
+	void reset(ANY* address = nullptr);
+	ANY* get() const;
+	ANY& operator*();
+	const ANY& operator*() const;
+	ANY& operator->();
+	const ANY& operator->()const;
+};
+template <typename T>
+void shared_ptr_test(T* address);
+}
+
+template<typename ANY>
+void my::Shared_ptr<ANY>::clear() {
+	all_ptrs.erase(data);
+	delete[] data;
+	data = nullptr;
+}
+
+template<typename ANY>
+my::Shared_ptr<ANY>::Shared_ptr() {
+	data = nullptr;
+}
+
+template<typename ANY>
+my::Shared_ptr<ANY>::Shared_ptr(ANY* address) {
+	reset(address);
+}
+
+
+template<typename ANY>
+my::Shared_ptr<ANY>::~Shared_ptr() {
+	if (data != nullptr) {
+		all_ptrs[data] -= 1;
+		if (all_ptrs[data] <= 0)
+			clear();
+	}
+}
+
+
+template<typename ANY>
+void my::Shared_ptr<ANY>::reset(ANY* address) {
+	if (data != nullptr) {
+		all_ptrs[data] -= 1;
+		if (all_ptrs[data] <= 0)
+			clear();
+	}
+	if (address == nullptr) return;
+
+	if (all_ptrs.count(data) > 0)
+		all_ptrs[data] += 1;
+
+	else
+		all_ptrs[data] = 1;
+
+	data = address;
+}
+
+template<typename ANY>
+ANY* my::Shared_ptr<ANY>::get() const {
+	return data;
+}
+
+template<typename ANY>
+ANY& my::Shared_ptr<ANY>::operator*() {
+	return *data;
+}
+
+template<typename ANY>
+const ANY& my::Shared_ptr<ANY>::operator*() const {
+	return *data;
+}
+
+//TODO: Добавить кое-что.
+
+template<typename ANY>
+ANY& my::Shared_ptr<ANY>::operator->() {
+	return *data;
+}
+
+template<typename ANY>
+const ANY& my::Shared_ptr<ANY>::operator->() const {
+	return *data;
+}
+
+template <typename T>
+void my::shared_ptr_test(T* address) {
+	my::Shared_ptr<T> ptr;
+	ptr.reset(address);
+	std::cout << "TEST: ptr (beg) = " << ptr.get() << std::endl;
+	std::cout << "TEST: *ptr (beg) = " << *ptr.get() << std::endl;
+	{
+		my::Shared_ptr<T> tmp(ptr.get());
+		std::cout << "TEST: tmp = " << tmp.get() << std::endl;
+		std::cout << "TEST: *tmp = " << *tmp.get() << std::endl;
+	}
+	std::cout << "TEST: ptr (end) = " << ptr.get() << std::endl;
+	std::cout << "TEST: *ptr (end) = " << *ptr.get() << std::endl;
+
+}
